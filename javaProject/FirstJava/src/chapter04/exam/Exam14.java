6c51da7f49d23ec3cf5d99dc7a2f22e79b8484b1
tackiljun <tack89826@gmail.com>
2023-03-24 18:15:03 +0900

    조건문,반목문 실습 코드 풀이.

---
+++ javaProject/FirstJava/src/chapter04/exam/Exam14.java
@@ -0,0 +1,43 @@
+package chapter04.exam;
+
+public class Exam14 {
+
+	public static void main(String[] args) {
+		
+				// 문제 14. 다음 식을 만족하는 조합을 찾는 프로그램 작성. 
+
+	      /*   A B
+
+	        +  B A
+
+	         ------	 
+	           9 9    // A+B=9  */
+			
+//		    int A = 0;
+//			int B = 0;
+//			
+//			for (A = 0; A < 10; A++) {
+//				for (B = 0; B < 10; B++) {
+//					
+//				}
+//			}
+		
+		
+		// 10A+B + 10B+A = 99 => 11A+11B=99 => A+B=9 => B=9-A / A=9-A.
+		
+		for(int i=0; i<10; i++) {
+			int a = i;
+			int b = 9-a;
+			
+			System.out.println("A: " + a + ", B: " + b);
+			
+		}
+		
+		
+			
+			
+		
+
+	}
+
+}
---
+++ javaProject/FirstJava/src/chapter04/exam/Exam16.java
@@ -0,0 +1,58 @@
+package chapter04.exam;
+
+public class Exam16 {
+
+	public static void main(String[] args) {
+		
+		//  문제16. 1 부터 99까지의 합을 구하는 프로그램 작성
+        //  while문, for문, do while 문을 각각 사용
+		
+		// while문
+//		int num = 0;
+//		int sum = 0;
+//		
+//		while (num <= 99) {
+//			sum += num;
+//			num++;
+//		}
+//		System.out.println(sum);
+//		
+//		
+//		System.out.println();
+//		System.out.println();
+//		
+//		
+//		// for문
+//		num = 0;
+//		sum = 0;
+//		
+//		for (num = 1; num <= 99; num++) {
+//			sum += num;
+//			num++;
+//		}
+//		System.out.println(sum);
+//		
+//		
+//		System.out.println();
+//		System.out.println();
+//		
+//		
+//		// do while문
+//		num = 0;
+//		sum = 0;
+//		
+//		do {
+//			sum +=num;
+//			num++;
+//		} while (num <= 99);
+//		System.out.println(sum);		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
