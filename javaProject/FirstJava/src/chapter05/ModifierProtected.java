061c7ad931de5f0f7a0ebaf4dcf0d96eef410c90
tackiljun <tack89826@gmail.com>
2023-03-28 18:19:27 +0900

    Create ModifierProtected.java

---
+++ javaProject/FirstJava/src/chapter05/ModifierProtected.java
@@ -0,0 +1,23 @@
+package chapter05;
+
+ public class ModifierProtected {     // (default) -> 같은 패키지 안에서만
+
+	
+	protected String name;
+	protected int age;
+	
+	protected void tell() {
+		System.out.println("안녕하세요. " + age + "살 " + name + " 입니다. ");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
