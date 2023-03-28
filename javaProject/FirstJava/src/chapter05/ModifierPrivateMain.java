a1a3e78d7fef58a67153c67dee802f379c99bc4e
tarkIlJun <tack89826@gmail.com>
2023-03-28 13:24:58 +0900

    chapter05

---
+++ javaProject/FirstJava/src/chapter05/ModifierPrivateMain.java
@@ -0,0 +1,26 @@
+package chapter05;
+
+public class ModifierPrivateMain {
+
+	public static void main(String[] args) {
+		
+		
+		ModifierPrivate mp = new ModifierPrivate("손흥민", 20);
+		
+		// mp.age = 20;
+		// mp.name = "손흥민";
+		
+		mp.tell();
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
