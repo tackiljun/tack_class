2ea2d75498b53847b51fb53fed97c5c41b5de566
tackiljun <tack89826@gmail.com>
2023-03-30 18:20:55 +0900

    Chapter7Review

---
+++ javaProject/FirstJava/src/chapter07/Review/SmartPhoneImpl.java
@@ -0,0 +1,22 @@
+package chapter07.Review;
+
+public class SmartPhoneImpl extends Phone{
+	
+	String model;
+	
+	public SmartPhoneImpl(String phoneNumber, String molel) {
+		super(phoneNumber);
+		this.model = model;
+	}
+	
+	@Override
+	void call() {
+		super.call();
+		System.out.println("이어팟을 이용해서 통화합니다.");
+	}
+	public static void main(String[] args) {
+		SmartPhoneImpl phone = new SmartPhoneImpl("010-2222-5555", "Google");
+		phone.call();
+	}
+
+}
---
+++ javaProject/FirstJava/src/chapter07/Review/SmartPhoneImpl2.java
@@ -0,0 +1,31 @@
+package chapter07.Review;
+
+public class SmartPhoneImpl2 extends Phone {
+	
+	String model;
+	
+	public SmartPhoneImpl2(String phoneNumber, String model) {
+		super(phoneNumber);
+		this.model = model;
+	}
+
+	@Override
+	void call() {
+		super.call();
+		System.out.println(model + "이어팟을 이용해서 통화합니다. ");
+	}
+	
+	public static void main(String[] args) {
+		
+		// 다형성 : 상위타입의 참조변수에 다양한 하위타입의 인스턴스를 참주할 수 있는것.
+		Phone p = new SmartPhoneImpl2("010-0000-1111", "IOS");
+		p.call(); // 하위 타입 인스턴스에 정의되어 있는 오버라이딩된 call( ) 호출된다!
+		
+		//p.playMusic();
+		
+		//SmartPhoneImpl2 p2 = new SmartPhoneImpl2("000", "test");
+		
+		SmartPhoneImpl2 p2 = (SmartPhoneImpl2)p2;
+		p2.playMusic();
+	}
+}
