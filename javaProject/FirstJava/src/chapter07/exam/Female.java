a76ac5b26e20885f5e86458e8c12b5cde5821b2a
tackiljun <tack89826@gmail.com>
2023-03-31 11:53:40 +0900

    chapter07examT

---
+++ javaProject/FirstJava/src/chapter07/exam/Female.java
@@ -0,0 +1,27 @@
+package chapter07.exam;
+
+public class Female extends Person {
+	
+	private String msg;
+
+	public Female(String name, String personNum) {
+		super(name, personNum);
+		this.msg = "안녕!";
+	}
+
+	public Female(String name, String personNum, String msg) {
+		super(name, personNum);
+		this.msg = msg;
+	}
+
+	@Override
+	public void printGreeting() {
+		super.printGreeting();
+		System.out.println(msg);
+	}
+	
+	
+	
+	
+
+}
---
+++ javaProject/FirstJava/src/chapter07/exam/Male.java
@@ -0,0 +1,25 @@
+package chapter07.exam;
+
+public class Male extends Person {
+	
+	String msg;
+
+	public Male(String name, String personNum, String msg) {
+		super(name, personNum);
+		this.msg = msg;
+	}
+
+	@Override
+	public void printGreeting() {
+		super.printGreeting();
+		System.out.println(msg);
+	}
+	
+	void hello() {
+		System.out.println("안녕하세요!!!!! ");
+	}
+	
+	
+	
+
+}
