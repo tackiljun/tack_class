9c69f444f93dffc8c3e2947566170210a71aa898
tarkIlJun <tack89826@gmail.com>
2023-04-02 17:28:06 +0900

    MyProject.Chapter07.Quiz7

---
+++ javaProject/MyProjetc/src/quiz07/Male.java
@@ -0,0 +1,28 @@
+package quiz07;
+
+public class Male extends Person {
+
+	// 2. Person 클래스를 상속해서 확장하는 새로운 클래스 Male 클래스와 Female 클래스를 정의 해봅시다. 
+	// ① 각 클래스는 상속 받은 멤버 외에 추가적인 변수와 메소드를 추가해서 새로운 클래스를 정의해봅시다.
+	// ② 각 클래스는 상속 받은 멤버 외에 추가적인 변수와 메소드를 추가해서 새로운 클래스를 정의해봅시다.
+	// ③ Person 클래스에서 정의된 인사하는 메소드를 오버라이딩 해봅시다.
+	// ④ Person 클래스에 생성자를 정의해서 인스턴스 변수들을 초기화 해봅시다.
+	
+	String say;
+	
+	public Male(String name, String number) {
+		super(name, number);
+		// TODO Auto-generated constructor stub
+		this.say = "안녕입니다.";
+	}
+
+	@Override
+	public void showPrint() {
+		// TODO Auto-generated method stub
+		super.showPrint();
+		System.out.println("요! ");
+	}
+	
+	
+
+}
