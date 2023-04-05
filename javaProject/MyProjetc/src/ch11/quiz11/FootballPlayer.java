d9afaddd3ee80a35c92df774e0be59b1dac25637
tackiljun <tack89826@gmail.com>
2023-04-05 19:16:14 +0900

    MyProject.ch11

---
+++ javaProject/MyProjetc/src/ch11/quiz11/FootballPlayer.java
@@ -0,0 +1,40 @@
+package ch11.quiz11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FootballPlayer {
+	
+	// 축구선수 클래스를 만들어 봅시다.
+	
+	String name;
+	int number;
+	String team;
+	int age;
+	// 1. 축구선수 인스턴스를 저장할 수 있는 List<E> 컬렉션 인스턴스를 생성해서 
+	// 인스턴스를 저장하고 출력하는 프로그램을 만들어 봅시다.
+	
+	public static void main(String[] args) {
+		
+		//List<Integer> numbers = new FootballPlayer<Integer>();
+		
+		List<FootballPlayer> players = new ArrayList<>();
+		FootballPlayer footballPlay1 = new FootballPlayer();
+		FootballPlayer footballPlay2 = new FootballPlayer();
+		FootballPlayer footballPlay3 = new FootballPlayer();
+		FootballPlayer footballPlay4 = new FootballPlayer();
+		FootballPlayer footballPlay5 = new FootballPlayer();
+		FootballPlayer footballPlay6 = new FootballPlayer();
+		FootballPlayer footballPlay7 = new FootballPlayer();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
