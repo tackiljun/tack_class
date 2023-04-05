086bcd413fa312cbdc4c38bf4234526c17b23b08
tackiljun <tack89826@gmail.com>
2023-04-05 17:16:26 +0900

    MyProject.Chapter10.Teacher

---
+++ javaProject/MyProjetc/src/exception/BadIdInputException.java
@@ -0,0 +1,17 @@
+package exception;
+
+public class BadIdInputException extends Exception {
+
+	public BadIdInputException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadIdInputException(String message) {
+		super(message); 
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
