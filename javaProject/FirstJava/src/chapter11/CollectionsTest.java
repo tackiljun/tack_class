86e694fe7c1d9519986c99df0c002d264c1a7da4
tackiljun <tack89826@gmail.com>
2023-04-06 18:12:35 +0900

    chapter11

---
+++ javaProject/FirstJava/src/chapter11/CollectionsTest.java
@@ -0,0 +1,42 @@
+package chapter11;
+
+import java.util.ArrayList; 
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class CollectionsTest {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> lottoNumbers = new ArrayList<>();
+		for(int i=0; i<6; i++) {
+			int n = new Random(System.nanoTime()).nextInt(45)+1;
+			lottoNumbers.add(n);
+		}
+		
+		System.out.println("최초 입력 데이터. ");
+		displayList(lottoNumbers);
+		
+		System.out.println("최대값 : " + Collections.max(lottoNumbers));
+		System.out.println("최소값 : " + Collections.min(lottoNumbers));
+		
+	    Collections.sort(lottoNumbers);
+	    displayList(lottoNumbers);
+	    
+	    Collections.reverse(lottoNumbers);
+	    displayList(lottoNumbers);
+	    
+	    Collections.shuffle(lottoNumbers);
+	    displayList(lottoNumbers);
+		
+	}
+	
+	static <E> void displayList(List<E> list) {
+		System.out.println("--------------------");
+		for(E num : list) {
+			System.out.println(num);
+		}
+	}
+
+}
---
+++ javaProject/FirstJava/src/chapter11/SmartPhoneHashMapTest.java
@@ -0,0 +1,34 @@
+package chapter11;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class SmartPhoneHashMapTest {
+
+	public static void main(String[] args) {
+		
+		HashMap<String, SmartPhone> hashmap = new HashMap<String, SmartPhone>();
+		
+		SmartPhone p = new SmartPhone("Spring", "010-1111-2222");
+		hashmap.put("ver", p);
+		
+		SmartPhone p2 = new SmartPhone("SON", "010-1111-2222");
+		hashmap.put("programName", p2);
+		//p.getPhoneNumber()
+		
+		System.out.println(hashmap.get("ver"));
+		System.out.println(hashmap.get("programName"));
+
+		
+		System.out.println("--------------------------------------------------------------");
+		
+		
+		Set<String> keySet = hashmap.keySet();
+		for(String keyname : keySet) {
+			System.out.println(keyname + " : " + hashmap.get(keyname));
+		}
+	}
+	
+	
+
+}
