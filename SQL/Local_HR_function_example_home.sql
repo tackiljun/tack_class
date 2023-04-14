9c0690137d49a82d74c88f1faaaec8d99ef1bfae
tackiljun <tack89826@gmail.com>
2023-04-14 11:17:27 +0900

    function.example

--- SQL/Local_HR_function_example_home.sql
+++ SQL/Local_HR_function_example_home.sql
@@ -6,11 +6,23 @@
 select ename, hiredate, substr(hiredate, 1, 5)
 from emp;
 
+-- T
+select hiredate, substr(hiredate,1,5), substr(hiredate,1,2)
+from emp
+where substr(hiredate,1,2)=81;
+
+
 -- 17. SUBSTR 함수를 사용하여 
 -- 4월에 입사한 사원을 출력하시오.
 select *
 from emp
 where substr(hiredate, 4, 2)=04;
+
+-- T
+select *
+from emp
+where substr(hiredate,4,2)=04;
+
 
 -- 18. MOD 함수를 사용하여 
 -- 사원번호가 짝수인 사람만 출력하시오.
@@ -18,11 +30,27 @@ select *
 from emp
 where mod(empno, 2)=0;
 
--- 19. 입사일을 년도는 2자리(YY), 
+-- T
+select *
+from emp
+where mod(empno,2)=0;
+
+
+-- 19. 입사일을 
+-- 년도는 2자리(YY), 
 -- 월은 숫자(MM)로 표시하고 
--- 요일은 약어 (DY)로 지정하여 출력하시오.
+-- 요일은 약어(DY)로 지정하여 출력하시오.
 select to_char(hiredate, 'YY-MM-DY')
 from emp;
+
+-- T
+select ename,
+       to_char(hiredate, 'YY') as "입사 년도",
+       to_char(hiredate, 'MM') as "입사 월",
+       to_char(hiredate, 'DY') as "입사 요일",
+       to_char(hiredate, 'YY.MM.DY') as "입사 년/월/요일"
+from emp;
+
 
 -- 20. 올해 몇 칠이 지났는지 출력하시오. 
 -- 현재날짜에서 올해 1월 1일을 뺀 결과를 출력하고 
@@ -30,11 +58,24 @@ from emp;
 select sysdate, between(sysdate, '23/01/01')
 from dual;
 
+-- T
+select sysdate, 
+       to_date('2023-01-01', 'YYYY-MM-DD'), 
+       sysdate - to_date('2023-01-01', 'YYYY-MM-DD'),
+       trunc(sysdate - to_date('2023-01-01', 'YYYY-MM-DD'))
+from dual;
+
+
 -- 21. 사원들의 상관 사번을 출력하되 
 -- 상관이 없는 사원에 대해서는 
 -- NULL 값 대신 0으로 출력하시오.
 select nvl(mgr, 0)
 from emp;
+
+-- T
+select mgr, nvl(mgr, 0)
+from emp;
+
 
 -- 22. DECODE 함수로 직급에 따라 
 -- 급여를 인상하도록 하시오. 
@@ -43,9 +84,18 @@ from emp;
 -- ‘MANAGER’인 사원은 150, 
 -- ‘CLERK”인 사원은 100을 인상하시오.
 select empno, ename, job, sal, nvl(decode(job, 'ANALYST', sal+200,
-                                               'SALESMAN', sal+100,
+                                               'SALESMAN', sal+180,
                                                'MANAGER', sal+150,
                                                'CLERK', sal+100), sal) as upsal
+from emp;
+
+-- T
+select ename, job, sal, 
+       nvl(decode(job, 'ANALYST', sal+200,
+                       'SALESMAN', sal+180,
+                       'MANAGER', sal+150,
+                       'CLERK', sal+100
+           ),sal) as up_sal
 from emp;
 
 
@@ -57,6 +107,10 @@ select  max(sal) as "최고액",
         round(avg(sal)) as "평균"
 from emp;
 
+-- T  
+select max(sal), min(sal), sum(sal), round(avg(sal))
+from emp;
+
 
 -- 24. 각 담당 업무 유형별로 
 -- 급여 최고액, 최저액, 총액 및 평균 액을 출력하시오. 
@@ -66,9 +120,14 @@ select job,
         min(sal) as "최저액", 
         sum(sal) as "총액",
         round(avg(sal)) as "평균"
-        from emp
-        -- order by job
-        group by job;
+from emp
+-- order by job
+group by job;
+
+-- T
+select job, max(sal), min(sal), sum(sal), round(avg(sal))
+from emp
+group by job;
         
 
 -- 25. COUNT(*) 함수를 이용하여 
@@ -78,15 +137,38 @@ select job, count(job)
 from emp
 group by job;
 
+-- T
+select job, count(*)
+from emp
+group by job;
 
--- 26. 관리자 수를 출력하시오.
+-- + 
+select deptno, job, count(*)
+from emp
+group by deptno, job
+order by deptno, job;
+
+
+-- 26. 관리자 수를 출력하시오. mgr => empno
 select count(mgr)
+from emp;
+
+-- T
+select ename, empno, mgr
+from emp
+order by mgr;
+
+select count(distinct mgr)
 from emp;
 
 
 -- 27. 급여 최고액, 급여 최저액의 
 -- 차액을 출력하시오.
-select max(sal), min(sal), max(sal)-min(sal) as "최고 최저 차액"
+select max(sal), min(sal), max(sal)-min(sal) as "차액"
+from emp;
+
+-- T
+select max(sal), min(sal), max(sal)-min(sal) 
 from emp;
 
 
@@ -101,15 +183,29 @@ group by job
 having not(min(sal)<2000)
 order by min(sal) desc;
 
+-- T
+select job, min(sal)
+from emp
+where mgr is not null
+group by job
+having min(sal)>=2000
+order by min(sal) desc;
 
--- 29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 
--- 모든 사원의 평균 급여를 출력하시오. 
+
+-- 29. 각 부서에 대해 
+-- 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오. 
 -- 평균 급여는 소수점 둘째 자리로 반올림 하시오.
 select deptno, 
        count(*) as "사원수", 
        round(avg(sal),2) as "평균급여"
 from emp
 group by deptno;
+
+-- T
+select deptno, count(*), round(avg(sal),2)
+from emp
+group by deptno
+order by deptno;
 
 
 -- 30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 
@@ -126,6 +222,25 @@ select deptno,
        40 , 'OPERATIONS') as "부서이름"
 from emp 
 group by deptno;
+
+-- T
+select * from dept;
+
+select deptno, 
+       decode(deptno, 10, 'ACCOUNTING',
+                      20, 'RESEARCH',
+                      30, 'SALES',
+                      40, 'OPERATIONS'
+       ) as DNAME,
+       decode(deptno, 10, 'NEW YORK',
+                      20, 'DALLAS',
+                      30, 'CHICAGO',
+                      40, 'BOSTON'
+       ) as LOC,
+       count(*), round(avg(sal))
+from emp
+group by deptno
+order by deptno;
 
 
 
