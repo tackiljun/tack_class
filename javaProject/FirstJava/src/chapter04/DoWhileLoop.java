6b6547d2216a5dd711cb8fa25ee0819cdac556bc
tackiljun <tack89826@gmail.com>
2023-04-20 16:08:35 +0900

    JAVA.Chapter04

--- javaProject/FirstJava/src/chapter04/DoWhileLoop.java
+++ javaProject/FirstJava/src/chapter04/DoWhileLoop.java
@@ -4,9 +4,9 @@ public class DoWhileLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		// do~while : 반드시 한번은 실행!
-		
+
 		int num = 10;
 		do {
 			System.out.println("A");
--- javaProject/FirstJava/src/chapter04/WhileLoop03.java
+++ javaProject/FirstJava/src/chapter04/WhileLoop03.java
@@ -4,16 +4,16 @@ public class WhileLoop03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int num = 0;
-		
-		while(true) {
-			
+
+		while (true) {
+
 			// 탈출 조건.
-			if(num==5) {
+			if (num == 5) {
 				break;
 			}
-			
+
 			System.out.println(num++);
 		}
 
