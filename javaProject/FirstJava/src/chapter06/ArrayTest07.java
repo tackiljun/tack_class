7fec54ed8665240731673e455fa1b0b1b747ca87
tackiljun <tack89826@gmail.com>
2023-04-20 16:15:08 +0900

    JAVA.chapter06

--- javaProject/FirstJava/src/chapter06/ArrayTest07.java
+++ javaProject/FirstJava/src/chapter06/ArrayTest07.java
@@ -1,16 +1,15 @@
 package chapter06;
 
 public class ArrayTest07 {
-	
+
 	public static void main(String[] args) {
-		
-		int[] score = {100, 90, 40, 50, 80};
-		
-		for(int num : score) {
+
+		int[] score = { 100, 90, 40, 50, 80 };
+
+		for (int num : score) {
 			System.out.println(num);
 		}
-		
-		
+
 	}
 
 }
