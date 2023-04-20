d403041db447cb85187411b1481ab5d53419ac39
tackiljun <tack89826@gmail.com>
2023-04-20 16:11:45 +0900

    JAVA.chapter05

--- javaProject/FirstJava/src/chapter05/ModifierPrivateMain.java
+++ javaProject/FirstJava/src/chapter05/ModifierPrivateMain.java
@@ -3,24 +3,14 @@ package chapter05;
 public class ModifierPrivateMain {
 
 	public static void main(String[] args) {
-		
-		
+
 		ModifierPrivate mp = new ModifierPrivate("손흥민", 20);
-		
+
 		// mp.age = 20;
 		// mp.name = "손흥민";
-		
+
 		mp.tell();
 
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
--- javaProject/FirstJava/src/chapter05/other/ModifierDefaultMain.java
+++ javaProject/FirstJava/src/chapter05/other/ModifierDefaultMain.java
@@ -6,13 +6,12 @@ public class ModifierDefaultMain {
 
 	public static void main(String[] args) {
 		
-		ModifierDefault md = new ModifierDefault() {
-			
-			md.name = "손흥민";
-			md.age = 20;
-			
-			md.tell();
-		}
+		ModifierDefault md = new ModifierDefault();
+		
+		//md.name = "손흥민";
+		//md.age = 20;
+		
+		//md.tell();
 
 	}
 
