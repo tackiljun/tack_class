f049156680bb548dc57bc6f4c4c7dd1b31a4f152
tackiljun <tack89826@gmail.com>
2023-04-20 16:14:15 +0900

    JAVA.chapter05.phone

--- javaProject/FirstJava/src/chapter05/phone/Member.java
+++ javaProject/FirstJava/src/chapter05/phone/Member.java
@@ -1,25 +1,24 @@
 package chapter05.phone;
 
 public class Member {
-	
+
 	// 변수.
 	String name;
 	String email;
 	int age;
 	String PhoneNumber;
 	SmartPhone phone;
-	
+
 	// Shift + Alt + s
-	
+
 	public Member(String name, String email, int age, String phoneNumber, SmartPhone phone) {
 		this.name = name;
 		this.email = email;
 		this.age = age;
 		PhoneNumber = phoneNumber;
-		this.phone = phone; 
+		this.phone = phone;
 	}
 
-	
 	public Member(String name, String email) {
 		this.name = name;
 		this.email = email;
@@ -27,18 +26,13 @@ public class Member {
 		this.PhoneNumber = "입력데이터가 없습니다.";
 		this.phone = new SmartPhone();
 	}
-	
+
 	void showData() {
-		System.out.println("이름 : " + name );
-		System.out.println("이메일 : " + email );
-		System.out.println("나이 : " + age );
-		System.out.println("전화번호 : " + PhoneNumber );
+		System.out.println("이름 : " + name);
+		System.out.println("이메일 : " + email);
+		System.out.println("나이 : " + age);
+		System.out.println("전화번호 : " + PhoneNumber);
 		this.phone.call();
 	}
-	
-	
-	
-	
-	
 
 }
