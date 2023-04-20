e26fe4a03872cf0d4fca0c331b4c558ef8145693
tackiljun <tack89826@gmail.com>
2023-04-20 18:19:48 +0900

    SQL.Local_HR

--- SQL/Local_HR_20230419_VIEW.sql
+++ SQL/Local_HR_20230419_VIEW.sql
@@ -4,6 +4,7 @@
 
 -- VIEW : 가상 테이블.
 -- VIEW 객체는 SQL 가진다! => SQL 실행결과를 가상의 테이블로 사용.
+
 select empno, ename, deptno 
 from emp
 where deptno=30
@@ -12,9 +13,10 @@ where deptno=30
 -- VIEW 객체 생성.
 -- CREATE OR REPLACE VIEW 뷰이름 AS 서브쿼리.
 create or replace view emp_view30
-as select empno, ename, deptno 
-   from emp
-   where deptno=30
+as 
+select empno, ename, deptno 
+from emp
+where deptno=30
 ;
 
 select * from emp_view30;
@@ -27,9 +29,10 @@ where o.bookid=b.bookid and o.custid=c.custid
 
 -- VIEW 생성.
 create or replace view order_info 
-as select o.orderid, c.name, b.bookname, b.price, o.saleprice, o.orderdate, c.phone
-   from orders o, book b, customer c
-   where o.bookid=b.bookid and o.custid=c.custid
+as 
+select o.orderid, c.name, b.bookname, b.price, o.saleprice, o.orderdate, c.phone
+from orders o, book b, customer c
+where o.bookid=b.bookid and o.custid=c.custid
 ;
 
 select * from order_info;
@@ -44,8 +47,13 @@ drop view emp_view30;
 select rownum, ename, hiredate from emp order by hiredate;
 
 create or replace view emp_hiredate_view
-as select ename, hiredate from emp order by hiredate
+as 
+select ename, hiredate 
+from emp 
+order by hiredate
 ;
+
+select rownum , ename, hiredate where rownum <= 3 from emp_hiredate_view;
 
 select rownum, ename, hiredate 
 from (select rownum, ename, hiredate from emp order by hiredate)
@@ -53,6 +61,8 @@ where rownum <= 3
 order by hiredate
 ;
 
-select rownum, ename, hiredate from emp_hiredate_view where rownum <= 3;
+select rownum, ename, hiredate 
+from emp_hiredate_view 
+where rownum <= 3;
 
 select to_char(hiredate, 'YYYY') from emp;
