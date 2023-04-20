2c9d616a36847cac30ecbe5a1c2eb9e45baf4f42
tackiljun <tack89826@gmail.com>
2023-04-20 16:02:40 +0900

    FirstJava.CageTest

--- javaProject/FirstJava/src/CafeTest/Quiz01.java
+++ javaProject/FirstJava/src/CafeTest/Quiz01.java
@@ -4,12 +4,12 @@ public class Quiz01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int num=120;
-        if(num>0) {
-            if((num%2)==0)
-                System.out.println("양수이면서 짝수");
-        }
+
+		int num = 120;
+		if (num > 0) {
+			if ((num % 2) == 0)
+				System.out.println("양수이면서 짝수");
+		}
 
 	}
 
--- javaProject/FirstJava/src/CafeTest/Quiz04.java
+++ javaProject/FirstJava/src/CafeTest/Quiz04.java
@@ -3,21 +3,20 @@ package CafeTest;
 public class Quiz04 {
 
 	public static void main(String[] args) {
-		
+
 		// SwitchBreak.java를 switch문이 아닌, if~else를 사용하는 형태로 변경해 보자.
-		
-		int n=3;
-        if (n==1) {
-            System.out.println("Simple Java"); 
-            } else if (n==2) {
-            	System.out.println("Funny Java");
-            } else if (n==3) {
-            	System.out.println("Fantastic Java");
-            } else {
-                System.out.println("The best programming language");
-        }
-        System.out.println("Do you like coffee?");
-		
+
+		int n = 3;
+		if (n == 1) {
+			System.out.println("Simple Java");
+		} else if (n == 2) {
+			System.out.println("Funny Java");
+		} else if (n == 3) {
+			System.out.println("Fantastic Java");
+		} else {
+			System.out.println("The best programming language");
+		}
+		System.out.println("Do you like coffee?");
 
 	}
 
