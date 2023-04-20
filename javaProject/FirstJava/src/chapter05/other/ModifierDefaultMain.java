5a514700f4c6c4bbb5d81e6028e8423c88d55a4f
tackiljun <tack89826@gmail.com>
2023-04-20 16:13:27 +0900

    JAVA.chapter05.other

--- javaProject/FirstJava/src/chapter05/other/ModifierDefaultMain.java
+++ javaProject/FirstJava/src/chapter05/other/ModifierDefaultMain.java
@@ -5,13 +5,13 @@ import chapter05.ModifierDefault;
 public class ModifierDefaultMain {
 
 	public static void main(String[] args) {
-		
+
 		ModifierDefault md = new ModifierDefault();
-		
-		//md.name = "손흥민";
-		//md.age = 20;
-		
-		//md.tell();
+
+		// md.name = "손흥민";
+		// md.age = 20;
+
+		// md.tell();
 
 	}
 
--- javaProject/FirstJava/src/chapter05/other/ModifierProtectedExtends.java
+++ javaProject/FirstJava/src/chapter05/other/ModifierProtectedExtends.java
@@ -7,22 +7,13 @@ public class ModifierProtectedExtends extends ModifierProtected {
 	// name, age, tell()
 
 	public static void main(String[] args) {
-		
+
 		ModifierProtectedExtends mpe = new ModifierProtectedExtends();
-		
+
 		mpe.name = "이강인";
 		mpe.age = 30;
-		
+
 		mpe.tell();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
