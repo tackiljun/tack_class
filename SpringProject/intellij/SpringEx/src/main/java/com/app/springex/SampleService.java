d42148e652233315d070f409e6dbaeaa516dd448
tackiljun <tack89826@gmail.com>
2023-05-09 19:13:53 +0900

    Spring/intellij/SpringEx

---
+++ SpringProject/intellij/SpringEx/src/main/java/com/app/springex/SampleService.java
@@ -0,0 +1,9 @@
+package com.app.springex;
+
+public class SampleService {
+
+    public void printTest() {
+        System.out.println("println 단축 텍스트는 ( sout 치고 + Ctrl + Space ) 입니다. ");
+    }
+
+}
