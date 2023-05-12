1cb5a3bedcf68dae5ea443fdc0d3707bb5a99409
tackiljun <tack89826@gmail.com>
2023-05-12 18:25:23 +0900

    Spring/MVC

---
+++ SpringProject/intellij/SpringMVC/src/main/java/com/hi/app/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.hi.app.controller;
+
+import com.hi.app.domain.OrderCommand;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@Log4j2
+@RequestMapping("/order/order")
+public class OrderController {
+
+
+    // Get : 입력 폼 화면.
+    @RequestMapping(method = RequestMethod.GET)
+    public String getOrderForm() {
+
+        log.info("GET /order/order");
+
+        return "order/orderForm";
+    }
+
+
+    // Post : 데이터를 받아서 화면에 결과를 출력하는 기능.
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String order(@ModelAttribute("order") OrderCommand orderCommand) {
+
+        log.info("POST /order/order");
+
+        log.info(orderCommand);
+
+        return "order/order";
+    }
+
+
+
+}
